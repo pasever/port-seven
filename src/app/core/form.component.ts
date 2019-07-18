73605a5ffa61c023308f15df4e9d0c3630134104
Evgheni Pascalov <evgheni@Evghenis-MacBook-Pro.local>
2019-07-18 14:27:12 -0400

    init new project

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -4,8 +4,6 @@ import { Product } from '../model/product.model';
 import { Model } from '../model/repository.model';
 import { MODES, SharedState} from './sharedState.model';
 
-// import { ActivatedRoute, Router } from '@angular/router';
-
 @Component({
   selector: 'paForm',
   templateUrl: 'form.component.html',
@@ -13,25 +11,13 @@ import { MODES, SharedState} from './sharedState.model';
 })
 export class FormComponent {
   product: Product = new Product();
-  originalProduct = new Product();
 
   constructor(
     private model: Model,
     private state: SharedState
-    // activeRoute: ActivatedRoute,
-    // private router: Router,
-  ) {
-    // activeRoute.params.subscribe(params => {
-    //   this.editing = params.mode == 'edit';
-    //   const id = params.id;
-    //   if (id != null) {
-    //     Object.assign(this.product, model.getProduct(id) || new Product());
-    //     Object.assign(this.originalProduct, this.product);
-    //   }
-    // });
-  }
+  ) { }
 
-  // editing = false;
+
   get editing(): boolean {
     return this.state.mode === MODES.EDIT;
   }
@@ -41,12 +27,6 @@ export class FormComponent {
       this.model.saveProduct(this.product);
       this.product = new Product();
       form.reset();
-      // this.originalProduct = this.product;
-      // this.router.navigateByUrl('/');
     }
   }
-
-  // resetForm() {
-  //    this.product = new Product();
-  // }
 }
--- src/app/core/table.component.ts
+++ src/app/core/table.component.ts
@@ -1,35 +1,19 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { Model } from '../model/repository.model';
-import { ActivatedRoute } from '@angular/router';
-// import { HighlightTrigger } from './table.animations';
 import { MODES, SharedState} from './sharedState.model';
 
 @Component({
   selector: 'paTable',
   templateUrl: 'table.component.html'
-  // animations: [HighlightTrigger]
 })
 export class TableComponent {
 
   constructor(
     private model: Model,
     private state: SharedState
-    // activeRoute: ActivatedRoute
   ) {
-    // activeRoute.params.subscribe(params => {
-    //   this.category = params.category || null;
-    // });
   }
-
-  // get categories(): string[] {
-  //   return this.model.getProducts()
-  //     .map(p => p.category)
-  //     .filter((category, index, array) => array.indexOf(category) === index);
-  // }
-  // category: string = null;
-  //
-  // highlightCategory = '';
 
   getProduct(key: number): Product {
     return this.model.getProduct(key);
@@ -38,11 +22,6 @@ export class TableComponent {
   getProducts(): Product[] {
     return this.model.getProducts();
  }
-
-  // getProducts(): Product[] {
-  //   return this.model.getProducts()
-  //     .filter(p => this.category === null || p.category === this.category);
-  // }
 
   deleteProduct(key: number) {
     this.model.deleteProduct(key);
@@ -58,8 +37,4 @@ export class TableComponent {
     this.state.mode = MODES.CREATE;
   }
 
-  // getRowState(category: string): string {
-  //   return this.highlightCategory === '' ? '' :
-  //     this.highlightCategory === category ? 'selected' : 'notselected';
-  // }
 }
