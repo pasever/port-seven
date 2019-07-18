6f0004565e34daf366e628746821d445624eeace
Evgheni Pascalov <evgheni@evghenis-mbp.duke-energy.com>
2019-07-18 16:35:57 -0400

    farther setup

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ModelModule } from '../model/model.module';
+import { TableComponent } from './table.component';
+import { FormComponent } from './form.component';
+import { SharedState } from './sharedState.model';
+
+
+@NgModule({
+    imports: [ BrowserModule, FormsModule, ModelModule ],
+    declarations: [TableComponent, FormComponent ],
+    providers: [ SharedState ],
+    exports: [ ModelModule, TableComponent, FormComponent ]
+})
+export class CoreModule { }
